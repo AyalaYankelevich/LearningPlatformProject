23252336e9bbabe78c7fe034b66534b85f8c1f96
Ayala <a0527152062@gmail.com>
2025-06-19 23:08:14 +0300

    add route to create admin

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authenticateToken = require('../middleware/authMiddleware'); // Import the middleware
 const adminController = require('../controllers/adminController');
 
+router.post('/admins', adminController.createAdmin);
 router.get('/users', authenticateToken, adminController.getAllUsers);                  // Route to get all users
 router.get('/users/:id/prompts', authenticateToken, adminController.getUserPrompts);  // Route to get prompts for a specific user
 
